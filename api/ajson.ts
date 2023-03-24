72a1aa6300643d0da865d297b7ffbcc541a1427d
zhangjiapeng04 <zhangjiapeng04@meituan.com>
2023-03-24 19:44:37 +0800

    feat: 添加三方库

--- api/ajson.ts
+++ api/ajson.ts
@@ -1,16 +1,24 @@
 import response from "../common/response";
+import axios from "axios";
 export const config = {
   runtime: 'edge',
 }
-
 export default async function handler(req) {
   const DataList: Array<any> = [];
+  // 发起一个post请求
+  axios({
+    method: 'post',
+    url: 'http://bigerfe.com',
+    data: {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    }
+  });
   for(let i=0;i<100;i++){
     DataList.push({
       id: i,
       name: `menuname ${i}`
     })
   }
-
   return response(DataList);
 }
